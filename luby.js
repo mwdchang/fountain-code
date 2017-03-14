d48fa67ef019c8fce2a1a8a04a4f0144bb951792
Daniel <mwdchang@gmail.com>
2017-03-14 18:21:34 -0400

    simple soliton distribution

--- luby.js
+++ luby.js
@@ -5,18 +5,44 @@ let _ = require('lodash');
 
 let c = 0;
 
+
+function basicSoliton(n) {
+  let r = Math.random();
+  for (let i=0; i < distribution.length; i++) {
+    if (r <= distribution[i]) return i+1;
+  }
+}
+
 function randomD(n) {
+  return basicSoliton(n);
+
+  /*
   c++;
-  if (c % 4 === 0) {
+  if (c % 5 === 0) {
     return 1;
   }
   return _.random(2, n);
+  */
 }
 
 
 
 let msg = process.argv[2];
+let msgLength = msg.length;
 let data = msg.split('').map((c,i) => { return {c:c, idx:i}});
+let distribution = [];
+
+distribution.push(1 / msgLength);
+let start = distribution[0]
+for (let i=2; i <= msgLength; i++) {
+  let v = start + ( 1 / (i*(i-1)) );
+  distribution.push( v );
+  start = v;
+}
+console.log('final start', start);
+console.log(distribution);
+
+
 
 let decodeMsg = new Array(msg.length);
 let reserve = [];
@@ -24,6 +50,7 @@ let reserve = [];
 
 function encode(data) {
   let sample = _.sampleSize(data, randomD(data.length));
+  // console.log('encode', sample.length);
 
   let packet = {
     data: 0,
@@ -38,16 +65,15 @@ function encode(data) {
 function decode(packet) {
 
   // 0) Strip the packet of any already decoded symbols
-  console.log('raw', packet);
+  // console.log('raw', packet);
   for (let i=0; i < decodeMsg.length; i++) {
     if (! decodeMsg[i] || packet.indicies.indexOf(i) === -1) continue;
 
     let c = decodeMsg[i].charCodeAt();
-    console.log(' > ', c);
     packet.data ^= c;
     _.remove(packet.indicies, (d) => d === i);
   }
-  console.log('stripped', packet);
+  // console.log('stripped', packet);
 
 
   // 1) Either resolve (length = 1) or put it into reserve for later
@@ -56,12 +82,12 @@ function decode(packet) {
 
     decodeMsg[idx] = String.fromCharCode(packet.data);
     // console.log('length 1 packet', decodeMsg[idx]);
-    console.log('going to resolve');
+    // console.log('going to resolve');
     resolve(packet.data, idx, 1);
   } else if (packet.indicies.length > 1) {
     reserve.push(packet);
   }
-  console.log('');
+  // console.log('');
 }
 
 
@@ -78,7 +104,7 @@ function resolve(data, idx, iter) {
   rlist.forEach(p => {
     let pidx = p.indicies[0];
     decodeMsg[pidx] = String.fromCharCode(p.data);
-    console.log('\t'.repeat(iter) + 'resolved ', decodeMsg[pidx]);
+    // console.log('\t'.repeat(iter) + 'resolved ', decodeMsg[pidx]);
     resolve(p.data, pidx, ++iter);
   });
 }
@@ -94,4 +120,4 @@ while (decodeMsg.join('').localeCompare(msg) !== 0 && c < 100) {
     decode(packet);
   }
 }
-console.log('Took ' + counter + ' packets.');
+console.log('Took ' + counter + ' packets. Original message lenth ' + msgLength);
